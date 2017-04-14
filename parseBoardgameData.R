765c4965c97d7678f2efdd41d129776c9cd3f14d
Zelbinian <dustin.l.hodge@gmail.com>
2017-04-14 16:29:39 -0700

    Method for querying a user's rated games

--- parseBoardgameData.R
+++ parseBoardgameData.R
@@ -60,6 +60,47 @@ retrieveAllUserNames <- function(req_url) {
   
 }
 
+getRatedGames <- function(username) {
+  
+  # stitch together the url for the api request
+  request <- paste0("https://www.boardgamegeek.com/xmlapi2/collection?", # api path for collection info
+                    "username=",username,                                # for this user
+                    "&rated=1",                                          # only rated games
+                    "&stats=1",                                          # full stats (including ratings)
+                    "&excludesubtype=boardgameexpansion")                # exclude expansions
+  
+  # The API works a little weirdly. Sometimes you have to wait for the server to
+  # retrieve the result. Unfortunately you can't use a callback or a promise. If the
+  # data is not ready yet, you get a <message> node telling you so. 
+  
+  repeat{
+    collection <- read_xml(request)
+    
+    if (length(xml_find_all(games_list, "//message")) == 0 ) {
+      
+      # sometimes the message warning might actually be an error, not a queuing response
+      if (length(xml_find_all(games_list, "//error")) > 0) {
+        
+        # in this case, throw a warning to the console...
+        warning(paste("The collection of rated games for user",
+                       username,
+                       "could not be found."))
+        
+        # .. and return an empty list
+        return(list())
+      }
+      
+      break
+    }
+    
+    # wait before re-requesting to minimize the number of times this loop will run
+    Sys.sleep(10) 
+  }
+  
+  return(collection)
+                    
+}
+
 ####################################################################################
 # STEP 1: Get the usernames of each member in the Redditors guild on BGG
 ####################################################################################
@@ -77,4 +118,5 @@ guild_usernames <- retrieveAllUserNames(guild_data_url)
 # STEP 2: Get each member's collection and, for each game they have rated, put the 
 # full details of that game in a data.frame
 ####################################################################################
+
 
